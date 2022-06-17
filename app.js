0d77c959d96555e5f61847dbb588a57f2d8470fa
Varsha Bharate <varsharanibharate@gmail.com>
2022-06-17 16:26:25 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+var req = require("request");
+const fs = require("fs");
+
+/* In this method, timedata is processes */
+function processTimeWebData() {
+  req("https://time.com", function (error, response, body) {
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+
+    var htmlString = body;
+
+    /* I am still working on parsing the html data */
+  });
+}
+
+app.get("/listUsers", function (req, res) {
+  processTimeWebData();
+});
+
+
+app.listen(3000, () => console.log("Server Running at http://localhost:3000"));
